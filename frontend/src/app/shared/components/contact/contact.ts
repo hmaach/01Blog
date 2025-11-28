4e5df40834d03098d9cf156f7ac0e19c3e6dbea2
Hamza Maach <hamzamaach56@gmail.com>
2025-11-28 19:14:20 +0100

    FEAT (frontend): add contact us form

--- frontend/src/app/shared/components/contact/contact.ts
+++ frontend/src/app/shared/components/contact/contact.ts
@@ -4,8 +4,10 @@ import {
   MatBottomSheetModule,
   MatBottomSheetRef,
 } from '@angular/material/bottom-sheet';
-import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastService } from '../../../core/services/toast.service';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -15,6 +17,65 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class Contact {
   private _bottomSheetRef = inject<MatBottomSheetRef<Contact>>(MatBottomSheetRef);
+
+  private toast = inject(ToastService);
+  private fb = inject(FormBuilder);
+
+  contactForm!: FormGroup;
+
+  personal = {
+    email: 'personal.email@example.com',
+    phone: '+1234567890',
+    location: 'Somewhere, Earth',
+    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/in',
+  };
+
+  ngOnInit() {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target as HTMLFormElement, {
+        publicKey: 'YOUR_PUBLIC_KEY',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        }
+      );
+    if (this.contactForm.valid) {
+      // emailjs
+      //   .sendForm(
+      //     'service_c06655j',
+      //     'template_yniiv5o',
+      //     document.forms['contactForm'],
+      //     'LKvBSb1l4zGQbi4Yy'
+      //   )
+      //   .then(
+      //     (result) => {
+      //       console.log(result.text);
+      //       this.toast.show('Message sent successfully!', 'success');
+      //       this.contactForm.reset(); // Reset form
+      //     },
+      //     (error) => {
+      //       console.log(error.text);
+      //       this.toast.show('Error sending message.', 'error');
+      //     }
+      //   );
+    }
+  }
 
   openLink(event: MouseEvent): void {
     this._bottomSheetRef.dismiss();
