a18a935dd598db289b277bd1c24ac518b482c2f7
Hamza Maach <hamzamaach56@gmail.com>
2025-11-29 12:00:20 +0100

    FEAT (frontend): complete contact us form and send the email

--- frontend/src/app/shared/components/contact/contact.ts
+++ frontend/src/app/shared/components/contact/contact.ts
@@ -1,17 +1,31 @@
 import { Component, inject } from '@angular/core';
-import {
-  MatBottomSheet,
-  MatBottomSheetModule,
-  MatBottomSheetRef,
-} from '@angular/material/bottom-sheet';
+import { MatBottomSheetModule, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ToastService } from '../../../core/services/toast.service';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-contact',
-  imports: [MatButtonModule, MatBottomSheetModule],
+  imports: [
+    MatButtonModule,
+    MatBottomSheetModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
@@ -19,62 +33,46 @@ export class Contact {
   private _bottomSheetRef = inject<MatBottomSheetRef<Contact>>(MatBottomSheetRef);
 
   private toast = inject(ToastService);
-  private fb = inject(FormBuilder);
 
   contactForm!: FormGroup;
 
-  personal = {
-    email: 'personal.email@example.com',
-    phone: '+1234567890',
-    location: 'Somewhere, Earth',
-    github: 'https://github.com',
-    linkedin: 'https://linkedin.com/in',
-  };
-
-  ngOnInit() {
-    this.contactForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      subject: ['', Validators.required],
-      message: ['', Validators.required],
+  ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      subject: new FormControl('', Validators.required),
+      message: new FormControl('', Validators.required),
     });
   }
 
-  onSubmit(e: Event) {
-    e.preventDefault();
+  onSubmit() {
+    if (!this.contactForm.valid) {
+      this.toast.show('Please fill out all fields correctly.', 'error');
+      return;
+    }
+
+    const formData = {
+      name: this.contactForm.get('name')?.value || '',
+      email: this.contactForm.get('email')?.value || '',
+      subject: this.contactForm.get('subject')?.value || '',
+      message: this.contactForm.get('message')?.value || '',
+    };
 
     emailjs
-      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target as HTMLFormElement, {
-        publicKey: 'YOUR_PUBLIC_KEY',
+      .send('service_zre7dod', 'template_nv001gf', formData, {
+        publicKey: 'LKvBSb1l4zGQbi4Yy',
       })
       .then(
         () => {
-          console.log('SUCCESS!');
+          console.log('Message sent successfully!');
+          this.toast.show('Message sent successfully!', 'success');
+          this._bottomSheetRef.dismiss();
         },
         (error) => {
+          this.toast.show('Error sending message.', 'error');
           console.log('FAILED...', (error as EmailJSResponseStatus).text);
         }
       );
-    if (this.contactForm.valid) {
-      // emailjs
-      //   .sendForm(
-      //     'service_c06655j',
-      //     'template_yniiv5o',
-      //     document.forms['contactForm'],
-      //     'LKvBSb1l4zGQbi4Yy'
-      //   )
-      //   .then(
-      //     (result) => {
-      //       console.log(result.text);
-      //       this.toast.show('Message sent successfully!', 'success');
-      //       this.contactForm.reset(); // Reset form
-      //     },
-      //     (error) => {
-      //       console.log(error.text);
-      //       this.toast.show('Error sending message.', 'error');
-      //     }
-      //   );
-    }
   }
 
   openLink(event: MouseEvent): void {
