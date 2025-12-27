e8d936a6a0184f4c1952b84e2dd63c503fbd07c2
Hamza Maach <hamzamaach56@gmail.com>
2025-12-27 18:08:11 +0100

    FEAT (frontend): implement admin guard

--- frontend/src/app/core/guards/admin.guard.ts
+++ frontend/src/app/core/guards/admin.guard.ts
@@ -1,14 +1,34 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { StorageService } from '../services/storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminGuard implements CanActivate {
-  constructor(private router: Router) {}
+  private storageService = inject(StorageService);
+  private router = inject(Router);
 
-  canActivate(): boolean | Observable<boolean> | Promise<boolean> {
-    return true;
+  private isBrowser: boolean;
+  constructor() {
+    this.isBrowser = typeof window !== 'undefined' && typeof window.localStorage !== 'undefined';
+  }
+
+  canActivate(): boolean {
+    // if (!this.isBrowser) {
+    //   return false;
+    // }
+
+    const role = this.storageService.getUserRole();
+
+    if (role === 'ADMIN') {
+      // console.log("is admin");
+      return true;
+    }
+    // console.log("is not admin");
+
+    this.router.navigate(['/feed']);
+    return false;
   }
 }
